870452f36f4cb7ae7c8567bd345b07226c151b4a
harynyainarajaonah01@gmail.com <harynyainarajaonah01@gmail.com>
2022-05-21 13:03:40 +0200

    dynamique setting

--- app/src/main/java/com/example/ecomania/view/ScoreFragment.java
+++ app/src/main/java/com/example/ecomania/view/ScoreFragment.java
@@ -14,10 +14,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.ecomania.R;
 import com.example.ecomania.controller.ScoreparthemeController;
 import com.example.ecomania.controller.ThemeController;
+import com.example.ecomania.utils.Constante;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,10 +38,16 @@ public class ScoreFragment extends Fragment {
 
     View view;
     ListView lst_score;
-    private ScoreparthemeController scoreparthemeController;
+    String url = Constante.url+"/joueur/score?id=";
+    TextView message;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_score, container, false);
+        lst_score = (ListView) view.findViewById(R.id.lst_score);
+        message = view.findViewById(R.id.message);
 
         //controle de l'existance de persistance
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(ScoreFragment.this.getContext());
@@ -37,24 +55,54 @@ public class ScoreFragment extends Fragment {
         if(user_id == null){
             Intent intent = new Intent(ScoreFragment.this.getContext(), LoginActivity.class);
             startActivity(intent);
+        }else{
+
+            //RESTAPI get score
+            RequestQueue requestQueue = Volley.newRequestQueue(this.getContext());
+            JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET, url+user_id, null,
+                    new Response.Listener<JSONObject>(){
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.e("error restapi get", response.toString());
+
+                            ArrayList<HashMap<String, String>> score = new ArrayList<HashMap<String,String>>();
+                            //avoir la liste
+                            try {
+                                Boolean error = response.getBoolean("error");
+                                if(error){
+                                    message.setText(response.getString("data"));
+                                }else{
+                                    JSONArray jsonArray = response.getJSONArray("data");
+                                    JSONObject item = new JSONObject();
+                                    for (int i=0; i < jsonArray.length(); i++){
+                                        item = jsonArray.getJSONObject(i);
+                                        HashMap<String,String> map = new HashMap<String,String>();
+                                        map.put("valeur", item.getString("valeur"));
+                                        map.put("pts", item.getString("score"));
+                                        score.add(map);
+                                    }
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                            //affichage de la liste
+                            SimpleAdapter adapter = new SimpleAdapter(view.getContext(), score, R.layout.score_item,
+                                    new String[]{"valeur", "pts"},
+                                    new int[]{R.id.lbl_theme, R.id.lbl_points}
+                            );
+                            lst_score.setAdapter(adapter);
+                        }
+                    },
+                    new Response.ErrorListener(){
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("error restapi get", error.toString());
+                        }
+                    }
+            );
+            requestQueue.add(objectRequest);
         }
-
-        this.scoreparthemeController = scoreparthemeController.getInstance();
-
-        // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_score, container, false);
-        lst_score = (ListView) view.findViewById(R.id.lst_score);
-
-        //static data
-        ArrayList<HashMap<String, String>> score = new ArrayList<HashMap<String,String>>();
-        score = this.scoreparthemeController.getScorepartheme();
-
-        //affichage de la liste
-        SimpleAdapter adapter = new SimpleAdapter(view.getContext(), score, R.layout.score_item,
-                new String[]{"valeur", "pts"},
-                new int[]{R.id.lbl_theme, R.id.lbl_points}
-        );
-        lst_score.setAdapter(adapter);
 
         return view;
     }
